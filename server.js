e724bb74eb7400f0b028a958ef1e6342006d9166
PujanShah22 <30696104+PujanShah22@users.noreply.github.com>
2017-08-28 14:17:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
+  
+  var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
