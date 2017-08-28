50394236b9f99983132e3162d4a8ce79a20762cb
PujanShah22 <30696104+PujanShah22@users.noreply.github.com>
2017-08-28 12:44:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/ui/article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/ui/article-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
